3c08bbad2d690700898de82f8c781f4d8cfb317d
henkjanborghuis <info@henkjanborghuis.nl>
2020-07-08 16:59:50 +0200

    Introducing Lists

---
+++ scripts/groups.py
@@ -0,0 +1,17 @@
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
+]
+# Your code here
+def converttostr(input_seq, seperator):
+    final_str = seperator.join(input_seq)
+    return final_str
+
+for group in musical_groups:
+    if len(group) == 3:
+        print(converttostr(group, ", "))
